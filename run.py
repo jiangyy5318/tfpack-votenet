18db9175f69fbe2b773ba8c372c214039fc6127b
jiangyy5318 <jiangyy5318@gmail.com>
2019-07-30 15:50:54 +0800

    changes.log

--- run.py
+++ run.py
@@ -6,9 +6,10 @@ import multiprocessing
 from evaluator import Evaluator, eval_mAP
 import six
 import numpy as np
+from tensorpack.train import SyncMultiGPUTrainerReplicated, TrainConfig, launch_train_with_config
+from tensorpack.utils.gpu import get_num_gpu
 
-BATCH_SIZE = 2
-
+BATCH_SIZE = 8
 
 def pad_along_axis(array, target_length, axis=0):
     pad_size = target_length - array.shape[axis]
@@ -84,22 +85,21 @@ class BatchData2Biggest(BatchData):
             for key in first_dp.keys():
                 data_list = [x[key] for x in data_holder]
                 if use_list:
-                    result[key] = data_list
+                    result[key] = data_listi
                 else:
                     result[key] = BatchData2Biggest._batch_numpy(data_list)
         return result
 
 
 if __name__ == '__main__':
-
-    tensorpack.utils.logger.auto_set_dir()
-
+    tensorpack.utils.logger.auto_set_dir(action='k')
     # this is the official train/val split
-    train_set = MyDataFlow('/media/neil/DATA/mysunrgbd', 'training', idx_list=list(range(5051, 10336)))
-
+    train_set = MyDataFlow('/data/jiangyy/sun_rgbd', 'train',
+                           idx_list=[int(e.strip()) for e in open('/data/jiangyy/sun_rgbd/train/train_data_idx.txt').readlines()][0:500])
+    #train_set = MyDataFlow('/data/jiangyy/sun_rgbd', 'train', idx_list=list(range(5051, 5551)))
     # dataset = BatchData(PrefetchData(train_set, 4, 4), BATCH_SIZE)
-
     lr_schedule = [(80, 1e-4), (120, 1e-5)]
+
     # lr_schedule = [(i, 5e-5) for i in range(260)]
     # get the config which contains everything necessary in a training
     config = AutoResumeTrainConfig(
@@ -113,11 +113,17 @@ if __name__ == '__main__':
             ModelSaver(),  # save the model after every epoch
             ScheduledHyperParamSetter('learning_rate', lr_schedule),
             # compute mAP on val set
-            PeriodicTrigger(Evaluator('/media/neil/DATA/mysunrgbd', 'training', 1, idx_list=list(range(1, 5051))), every_k_epochs=1, before_train=False),
+            PeriodicTrigger(Evaluator('/data/jiangyy/sun_rgbd', 'train', 1,
+                                      idx_list=[int(e.strip()) for e in open('/data/jiangyy/sun_rgbd/train/val_data_idx.txt').readlines()])
+                            , every_k_epochs=20, before_train=False),
+            # MaxSaver('val_accuracy'),  # save the model with highest accuracy
             # MaxSaver('val_accuracy'),  # save the model with highest accuracy
         ],
         # steps_per_epoch=100,
-        max_epoch=260,
+        max_epoch=250,
     )
-    launch_train_with_config(config, SimpleTrainer())
+    #trainer = SyncMultiGPUTrainerReplicated(max(get_num_gpu(), 1))
+    trainer = SyncMultiGPUTrainerParameterServer(max(get_num_gpu(), 1))
+    #trainer = SimpleTrainer()
+    launch_train_with_config(config, trainer)
 
